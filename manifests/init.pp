91bf2dc7d48d83d6b9a738bac1c4a6b61550f40a
Will Farrington <wfarr@github.com>
2013-06-14 17:37:35 -0700

    Use GCC 4.8 under Mavericks

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,13 +4,41 @@
 #
 #   include gcc
 class gcc {
-  include homebrew
 
-  homebrew::formula { 'apple-gcc42':
-    before => Package['boxen/brews/apple-gcc42'],
+  case $::operatingsystem {
+    'Darwin': {
+      include homebrew
+
+      case $::macosx_productversion_major {
+        '10.8': {
+          homebrew::formula { 'apple-gcc42':
+            before => Package['boxen/brews/apple-gcc42'],
+          }
+
+          package { 'boxen/brews/apple-gcc42':
+            ensure => '4.2.1-5666.3-boxen1'
+          }
+        }
+
+        '10.9': {
+          exec { 'brew tap homebrew/versions':
+            creates => "${boxen::config::home}/homebrew/Library/Taps/homebrew-versions"
+          }
+
+          package { 'homebrew/versions/gcc48':
+            require => Exec['brew tap homebrew/versions']
+          }
+        }
+
+        default: {
+          # noop
+        }
+      }
+    }
+
+    default: {
+      package { 'gcc': }
+    }
   }
 
-  package { 'boxen/brews/apple-gcc42':
-    ensure => '4.2.1-5666.3-boxen1'
-  }
 }
