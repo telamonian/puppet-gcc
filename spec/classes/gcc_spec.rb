9ed42291de5756a836376a71ad7987ee15a608d3
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2014-06-25 18:20:02 -0300

    Update GCC48 version to 4.8.3 and add boxen prefix

--- spec/classes/gcc_spec.rb
+++ spec/classes/gcc_spec.rb
@@ -4,6 +4,8 @@ describe 'gcc' do
   let(:facts) { default_test_facts.merge(:macosx_productversion_major => 10.9) }
 
   it do
-    should contain_package('boxen/brews/gcc48')
+    should contain_package('boxen/brews/gcc48').with({
+      :ensure => '4.8.3-boxen1'
+    })
   end
 end
